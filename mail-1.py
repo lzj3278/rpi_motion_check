bd5eb47f2836beb681d6c402decfb7a844740965
lzj <zhongjie.li@viziner.cn>
2016-12-09 16:54:59 +0800

    1

--- mail-1.py
+++ mail-1.py
@@ -7,35 +7,35 @@ from email.header import Header
 from email.mime.multipart import MIMEMultipart
 
 # 第三方 SMTP 服务
-mail_host="smtp.126.com"  #设置服务器
-mail_user="lzj3278@126.com"    #用户名
-mail_pass="324ldj3fat"   #口令 
+mail_host = "smtp.exmail.qq.com"  # 设置服务器
+mail_user = "zhongjie.li@viziner.cn"  # 用户名
+mail_pass = "V1z1ner!"  # 口令
 
 
-sender = 'lzj3278@126.com'
-receivers = ['zhongjie.li@viziner.cn']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+sender = 'zhongjie.li@viziner.cn'
+receivers = ['lzj3278@126.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
 message = MIMEMultipart()
 #message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
 message['From'] = Header("李忠杰", 'utf-8')
-message['To'] =  Header("测试", 'utf-8')
+message['To'] = Header("测试", 'utf-8')
 
 subject = 'Python SMTP 邮件测试'
 message['Subject'] = Header(subject, 'utf-8')
-message.attach(MIMEText('这是菜鸟教程Python 邮件发送测试……', 'plain', 'utf-8'))
-att1 = MIMEText(open('wallhaven.jpg', 'rb').read(), 'base64', 'utf-8')
-att1["Content-Type"] = 'application/octet-stream'
-att1["Content-Disposition"] = 'attachment; filename="test.jpg"'
-message.attach(att1)
+message.attach(MIMEText('这是Python 邮件发送测试……', 'plain', 'utf-8'))
+# att1 = MIMEText(open('wallhaven.jpg', 'rb').read(), 'base64', 'utf-8')
+# att1["Content-Type"] = 'application/octet-stream'
+# att1["Content-Disposition"] = 'attachment; filename="test.jpg"'
+# message.attach(att1)
 
 
 try:
-    smtpObj = smtplib.SMTP() 
+    smtpObj = smtplib.SMTP()
     smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
-    #smtpObj.starttls()
-    smtpObj.login(mail_user,mail_pass)  
+    # smtpObj.starttls()
+    smtpObj.login(mail_user, mail_pass)
     smtpObj.sendmail(sender, receivers, message.as_string())
     print "邮件发送成功"
-except smtplib.SMTPException,e:
+except smtplib.SMTPException, e:
     print e
     print "Error: 无法发送邮件"
